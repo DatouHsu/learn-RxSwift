f024a692c9a918691bf447b055ef517ed25a00e9
DatouHsu <howrudatou@gmail.com>
2017-08-17 02:21:12 +0800

    prepare for combine observable

--- GreetingGenerator/GreetingGenerator/ViewController.swift
+++ GreetingGenerator/GreetingGenerator/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     case useTextField
   }
 
+  let disposeBag = DisposeBag()
+  let lastSelectedGreeting: Variable<String> = Variable("Hello")
+
   @IBOutlet weak var stateSegmentControl: UISegmentedControl!
   @IBOutlet weak var greetingsLabel: UILabel!
   @IBOutlet weak var greetingsTextField: UITextField!
@@ -28,14 +31,44 @@ class ViewController: UIViewController {
     super.viewDidLoad()
 
     let nameObservible: Observable<String?> = nameTextField.rx.text.asObservable()
-    nameObservible.subscribe({ (event: Event<String?>) in
-      switch event {
-      case .completed: print("Complete")
-      case .error(_): print("Error")
-      case .next(let string): print("\(string)")
-      }
-    })
-  }
+    let greetingObservible: Observable<String?> = greetingsTextField.rx.text.asObservable()
+    // MARK: few way to do the same thing
+//    nameObservible.subscribe({ (event: Event<String?>) in
+//      switch event {
+//      case .completed: print("Complete")
+//      case .error(_): print("Error")
+//      case .next(let string): print("\(string)")
+//      }
+//    })
 
+//    nameObservible.subscribe(onNext: { (string: String?) in
+//      self.greetingsLabel.text = string
+//    })
+//    nameObservible.bind(to: greetingsLabel.rx.text).addDisposableTo(disposeBag)
+
+    let greetingWithNameObservible: Observable<String?> = Observable.combineLatest(nameObservible, greetingObservible) { (string1: String?, string2: String?) -> String? in
+      return string1! + ", " + string2!
+      }
+    greetingWithNameObservible.bind(to: greetingsLabel.rx.text).addDisposableTo(disposeBag)
+
+    let segmentControlObservible: Observable<Int> = stateSegmentControl.rx.value.asObservable()
+    let stateObservable: Observable<State> = segmentControlObservible.map { (selectIndex: Int) -> State in
+      return State(rawValue: selectIndex)!
+    }
+    let greetingTextFieldEnableObservable: Observable<Bool> = stateObservable.map { (state: State) -> Bool in
+      return state == .useTextField
+    }
+    greetingTextFieldEnableObservable.bind(to: greetingsTextField.rx.isEnabled).addDisposableTo(disposeBag)
+
+    let buttonEnableObservable: Observable<Bool> = greetingTextFieldEnableObservable.map { (greetingEnable: Bool) -> Bool in
+      return !greetingEnable
+    }
+    greetingButtons.forEach { (button) in
+      buttonEnableObservable.bind(to: button.rx.isEnabled).addDisposableTo(disposeBag)
+      button.rx.tap.subscribe(onNext: { (nothing: Void) in
+        self.lastSelectedGreeting.value = button.currentTitle!
+      }).addDisposableTo(disposeBag)
+    }
+  }
 }
 
