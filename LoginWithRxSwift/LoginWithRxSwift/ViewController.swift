322f988ee4c5d04adf32f02919b3d10fe973504c
DatouHsu <howrudatou@gmail.com>
2017-09-03 16:54:01 +0800

    init new login practice

---
+++ LoginWithRxSwift/LoginWithRxSwift/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  LoginWithRxSwift
+//
+//  Created by DatouHsu on 9/3/17.
+//  Copyright © 2017 datouHsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var usernameTextField: UITextField!
+  @IBOutlet weak var usernameLabel: UILabel!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var passwordLabel: UILabel!
+  @IBOutlet weak var repeatPasswordTextField: UITextField!
+  @IBOutlet weak var repeatPasswordLabel: UILabel!
+
+  @IBOutlet weak var registerButton: UIButton!
+  @IBOutlet weak var loginButton: UIBarButtonItem!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
